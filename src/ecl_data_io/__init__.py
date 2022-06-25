75f1941619d34d1e3379a65b333c3ac4c9c1f191
Eivind Jahren <ejah@equinor.com>
2022-06-25 19:15:07 +0200

    Export MESS from ecl_data_io

--- src/ecl_data_io/__init__.py
+++ src/ecl_data_io/__init__.py
@@ -2,6 +2,7 @@ import ecl_data_io.version
 
 from .format import Format
 from .read import lazy_read, read
+from .types import MESS
 from .write import write
 
 __author__ = "Equinor"
@@ -9,4 +10,4 @@ __email__ = "fg_sib-scout@equinor.com"
 
 __version__ = ecl_data_io.version.version
 
-__all__ = ["read", "lazy_read", "write", "Format"]
+__all__ = ["read", "lazy_read", "write", "Format", "MESS"]
