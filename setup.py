8666f1022825ebe6d599f2f06ea7b1504ac50e12
Eivind Jahren <ejah@equinor.com>
2022-06-25 21:22:03 +0200

    Add support for python 3.10

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     setup_requires=["setuptools_scm"],
     include_package_data=True,
